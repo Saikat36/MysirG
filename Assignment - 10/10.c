c798ff40c76e3f25637488251a173fe3d3e58358
Saikat36 <saikatp360@gmail.com>
2022-11-20 17:37:22 +0530

    added

--- Assignment - 10/10.c
+++ Assignment - 10/10.c
@@ -1,10 +1,41 @@
 /* write a programe to print all Armstrong numbers under 1000 */
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 int main()
 {
+    int x, y, n = 0, sum = 0, d, p, i;
+
+    for (x = 0; x <= 1000; x++)
+    {
+
+        y = x;
+        n = 0;
+
+        while (y)
+        {
+            y = y / 10;
+            n++;
+        }
+
+        for (y = x, sum = 0;     y  ;  y = y / 10)
+        {
+            d = y % 10;
+
+            for (p = 1, i = 1; i <= n; i++)
+            {
+                p = p * d;
+            }
+            sum = sum + p;
+        }
+
+        if (sum == x)
+        {
+            printf("%d is an armstrong number\n", x);
+        }
+    }
+
     printf("\n");
     return 0;
 }
--- Assignment - 10/9.c
+++ Assignment - 10/9.c
@@ -1,10 +1,40 @@
 /* write a programe to check wheather a given number is an Armstrong number or not */
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 int main()
 {
+    int x, y, n = 0, sum = 0, d, p, i;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    y = x;
+    while (y)
+    {
+        y = y / 10;
+        n++;
+    }
+
+    for (y = x; y; y = y / 10)
+    {
+        d = y % 10;
+
+        for (p = 1, i = 1; i <= n; i++)
+        {
+            p = p * d;
+        }
+        sum = sum + p;
+    }
+
+    if (sum == x)
+    {
+        printf("%d is an armstrong number", x);
+    }
+    else
+    {
+        printf("%d is not an armstrong number", x);
+    }
+
     printf("\n");
     return 0;
 }
